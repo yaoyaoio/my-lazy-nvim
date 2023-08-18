a8511ea5f7e69b89462cdd90e034333b898dc6a2
yaoyao <i@yaoyao.io>
2023-08-18 20:36:43 +0800

    fist commit

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add sainnhe/sonokai
+  { "sainnhe/sonokai" },
+
+  -- 配置 LazyVim 以加载 sonokai
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "sonokai",
+    },
+  },
+}
